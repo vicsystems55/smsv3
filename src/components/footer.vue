c3c25d31ce98b40580625d98c7420d3999295018
vicSystems <victorasuquob@gmail.com>
2022-08-25 12:04:05 +0100

    version 2

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,7 +4,7 @@
           <div class="container-fluid">
             <div class="row">
               <div class="col-md-12 footer-copyright text-center">
-                <p class="mb-0">Copyright 2020 © Cuba theme by pixelstrap  </p>
+                <p class="mb-0">Copyright 2022 © vicSystems Technologies Ltd.  </p>
               </div>
             </div>
           </div>
